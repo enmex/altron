df44583f900a5535a71d4d2c80c45360c7010514
Michael Privalov <privalovmihail2@gmail.com>
2025-09-24 14:24:38 +0600

    hotfix

--- services/config/sftp_config.go
+++ services/config/sftp_config.go
@@ -9,7 +9,7 @@ import (
 func NewSFTPConfig() *sftp.Config {
 	return &sftp.Config{
 		APIServer:     fmt.Sprintf("%s:8080", "altron.sftp.loc"), //os.Getenv("SFTP_SFTPGO_SERVICE_HOST")),
-		Server:        fmt.Sprintf("%s:21", "altron.sftp.loc"), //os.Getenv("SFTP_SFTPGO_SERVICE_HOST")),
+		Server:        fmt.Sprintf("%s:2022", "altron.sftp.loc"), //os.Getenv("SFTP_SFTPGO_SERVICE_HOST")),
 		User:          strings.ReplaceAll("altron", "-", ""),
 		Password:      strings.ReplaceAll("altron", "-", ""),
 		AdminPassword: "admin", //os.Getenv("SFTP_ADMIN_PASS"),
